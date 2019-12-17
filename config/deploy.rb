48f2fdbab17ae01d24b1b93b39903874fee302f0
Bèr Kessels <ber@berk.es>
2019-12-17 11:35:48 +0100

    Include capistrano tasks that reload the services after deploying.
    After a successfull deploy, I need the services to switch over to use the
    new code. I need to restart the services.
    
    These capistrano tasks do that.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,30 @@ set :branch, ENV.fetch('BRANCH', 'master')
 set :application, 'mastodon'
 set :migration_role, :app
 
-append :linked_dirs, 'vendor/bundle', 'node_modules', 'public/system'
+
+namespace :systemd do
+  %i[sidekiq streaming web].each do |service|
+    desc "Reload #{service} service"
+    task "#{service}:reload".to_sym do
+      on roles(:app) do
+        systemctl :reload, service
+      end
+    end
+
+    desc "Show the status of #{service} service"
+    task "#{service}:status".to_sym do
+      on roles(:app) do
+        systemctl :status, service
+      end
+    end
+  end
+
+  def systemctl(action, service)
+    # runs e.g. "sudo restart mastodon-sidekiq.service"
+    sudo :systemctl, action, "#{fetch(:application)}-#{service}.service"
+  end
+end
+
+after 'deploy:publishing', 'systemd:web:reload'
+after 'deploy:publishing', 'systemd:sidekiq:reload'
+after 'deploy:publishing', 'systemd:streaming:reload'
