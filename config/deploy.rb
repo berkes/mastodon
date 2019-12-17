e24342c8a55d2c6d0340aa7c5f7a1f6df1a34fb5
Bèr Kessels <ber@berk.es>
2019-12-17 11:37:53 +0100

    Load env vars before running all cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,32 @@ set :branch, ENV.fetch('BRANCH', 'master')
 set :application, 'mastodon'
 set :migration_role, :app
 
+append :linked_dirs, 'vendor/bundle', 'public/system'
+
+namespace :env_vars do
+  desc 'Load environment variables'
+  task :load do
+    # Grab the current value of :default_env
+    environment = fetch(:default_env, {})
+    on roles(:app) do
+      # Read in the environment file
+      lines = capture("cat #{shared_path}/env")
+      lines.each_line do |line|
+        # Clean up the input by removing line breaks, tabs etc
+        line = line.gsub /[\t\r\n\f]+/, ""
+        # Grab the key and value from the line
+        key, value = line.split("=")
+        # Remove surrounding quotes if present
+        value = value.slice(1..-2) if value.start_with?('"') and value.end_with?('"')
+        # Store the value in our :default_env copy
+        environment.store(key, value)
+      end
+
+      # Finally, update the global :default_env variable again
+      set :default_env, environment
+    end
+  end
+end
 
 namespace :systemd do
   %i[sidekiq streaming web].each do |service|
@@ -35,3 +61,5 @@ end
 after 'deploy:publishing', 'systemd:web:reload'
 after 'deploy:publishing', 'systemd:sidekiq:reload'
 after 'deploy:publishing', 'systemd:streaming:reload'
+
+after 'deploy:starting', 'env_vars:load'
